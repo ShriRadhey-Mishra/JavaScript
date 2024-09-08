7889e8c39c4a51ad2f9de3622a0621ff30b14c77
Shri Radhey Mishra <145862558+Shri-Radhey-Mishra@users.noreply.github.com>
2024-09-08 18:15:31 +0530

    Create assignment_operator.js

---
+++ Basics/Operators/assignment_operator.js
@@ -0,0 +1,15 @@
+let x = 2;
+console.log("Value of x here is " + x);
+console.log("Addition-Assignment Operator: x += 2 = " + (x += 2));
+
+console.log("Value of x here is " + x);
+console.log("Subtraction-Assignment Operator: x -= 2 = " + (x -= 2));
+
+console.log("Value of x here is " + x);
+console.log("Multiplication-Assignment Operator: x *= 2 = " + (x *= 2));
+
+console.log("Value of x here is " + x);
+console.log("Division-Assignment Operator: x /= 2 = " + (x /= 2));
+
+console.log("Value of x here is " + x);
+console.log("Modulus-Assignment Operator: x %= 2 = " + (x %= 2));
