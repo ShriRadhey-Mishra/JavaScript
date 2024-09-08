68eba58e623228ca5ac56ab846689f7d3c559cfc
Shri Radhey Mishra <145862558+Shri-Radhey-Mishra@users.noreply.github.com>
2024-09-08 18:09:26 +0530

    Create arithmetic_operators.js

---
+++ Basics/Operators/arithmetic_operators.js
@@ -0,0 +1,17 @@
+let x = 2;
+let y = 8;
+
+console.log("Addition Operator: x + y = " + (x+y));
+console.log("Subtraction Operator: x - y = " + (x-y));
+console.log("Multiplication Operator: x * y = " + (x*y));
+console.log("Division Operator: x / y = " + (x/y));
+console.log("Modulus Operator: x % y = " + (x%y));
+console.log("Exponentiation Operator: x ** y = " + (x**y));
+
+// Increment Operator
+console.log("Pre-increment Operator: ++x = " + (++x));
+console.log("Post-increment Operator: x++" + (x++));
+
+// Decrement Operator
+console.log("Pre-decrement Operator: --x = " + (--x));
+console.log("Post-decrement Operator: x--" + (x--));
