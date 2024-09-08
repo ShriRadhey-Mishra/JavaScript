2edcfdc7053cb88d9fe6fe30b576bb77cd470a7c
Shri Radhey Mishra <145862558+Shri-Radhey-Mishra@users.noreply.github.com>
2024-09-08 18:40:26 +0530

    Create equality_operator.js

---
+++ Basics/Operators/equality_operator.js
@@ -0,0 +1,8 @@
+// Strict Equality (Checks both Type and Value)
+console.log("1 === 1: " + (1 === 1));
+console.log("\"1\" === 1: " + ("1" === 1));
+
+//  Loose Equality (Checks only the value)
+console.log("1 == 1: " + (1 == 1));
+console.log("\"1\" == 1: " + ("1" == 1));
+console.log("true == 1: " + (true == 1));
