16758b708073d848d289230586a275cb8e0bcca9
Shri Radhey Mishra <145862558+Shri-Radhey-Mishra@users.noreply.github.com>
2024-09-08 18:22:55 +0530

    Create comparison_operator.js

---
+++ Basics/Operators/comparison_operator.js
@@ -0,0 +1,11 @@
+let x = 1;
+
+// Relational
+console.log("Greater than Comparison Operator: Is x > 0: " + (x > 0));
+console.log("Greater than or equal to Comparison Operator: Is x >= 0: " + (x >= 1));
+console.log("Less than Comparison Operator: Is x < 0: " + (x < 1));
+console.log("Less than or equal to Comparison Operator: Is x <= 0: " + (x <= 1));
+
+// Equality
+console.log("Equality Operator: Is x === 0: " + (x === 1));
+console.log("Not Equality Operator: Is x !== 0: " + (x !== 1));
