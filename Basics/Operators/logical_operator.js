edca2dfa540f46a913a58b44e6a049bc61c3b23e
Shri Radhey Mishra <145862558+Shri-Radhey-Mishra@users.noreply.github.com>
2024-09-08 18:54:27 +0530

    Create logical_operator.js

---
+++ Basics/Operators/logical_operator.js
@@ -0,0 +1,19 @@
+// Logical AND Operator (&&)
+console.log("Logical AND table: ")
+console.log("true && true = " + (true && true));
+console.log("false && true = " + (false && true));
+console.log("true && false = " + (true && false));
+console.log("false && false = " + (false && false));
+console.log("\n");
+
+// Logical OR Operator (||)
+console.log("Logical AND table: ")
+console.log("true || true = " + (true || true));
+console.log("false || true = " + (false || true));
+console.log("true || false = " + (true || false));
+console.log("false || false = " + (false || false));
+console.log("\n");
+
+// Logical NOT Operator (!)
+console.log("!true = " + (!true));
+console.log("!false = " + (!false));
