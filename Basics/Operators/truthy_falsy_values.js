7ec0d3136c7b8fc7ad2e1b08466b30d0ad45c215
Shri Radhey Mishra <145862558+Shri-Radhey-Mishra@users.noreply.github.com>
2024-09-08 19:14:05 +0530

    Create truthy_falsy_values.js

---
+++ Basics/Operators/truthy_falsy_values.js
@@ -0,0 +1,9 @@
+console.log("false || true = ", (false||true));
+console.log("false || \"Anjali\" = ", (false||"Anjali"));
+console.log("false || 1 = ", (false||1));
+
+// Falsy Values (acting false values) includes undefined, null, 0, false, '', NaN
+// Anything that is not Falsy -> Truthy Value
+
+// Short-Curcuiting, here only first logical operation is performed killing the other operations in front.
+console.log("false || 1 || 2 =", (false || 1 || 2));
