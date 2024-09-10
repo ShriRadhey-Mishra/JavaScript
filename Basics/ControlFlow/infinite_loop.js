ec00ce1f912c7b3ee8a60f2d6f7b56f1ddcb75e9
Shri Radhey Mishra <148370155+ShriRadhey-Mishra@users.noreply.github.com>
2024-09-10 22:38:18 +0530

    added infinite_loop.js

---
+++ Basics/ControlFlow/infinite_loop.js
@@ -0,0 +1,19 @@
+let i = 11;
+
+while (i > 10) {
+    console.log(i);
+}
+
+while (true) {
+    console.log("Hello!");
+}
+
+do {
+    console.log("Hello!");
+} while (true)
+
+for (let i = 0; i < 10; ) {
+    console.log("Hello!")
+}
+
+// We usually try to avoid infinite loops, which usually occurs due to not paying attention.
