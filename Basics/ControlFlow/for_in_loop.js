50d905021b765ff75cd27afce6e2a06849c87d03
Shri Radhey Mishra <148370155+ShriRadhey-Mishra@users.noreply.github.com>
2024-09-10 22:47:22 +0530

    addded for_in_loop.js

---
+++ Basics/ControlFlow/for_in_loop.js
@@ -0,0 +1,16 @@
+// It is used to iterate over the properties of an object or elements in an array.
+
+let person = {
+    name: 'Anjali',
+    age: 18
+};
+
+for (let key in person) {
+    console.log(`${key}: ${person[key]}`);
+}
+
+let colors = ['red', 'green', 'blue'];
+
+for (let index in colors) {          // It gives index here
+    console.log(index, colors[index]);
+}
