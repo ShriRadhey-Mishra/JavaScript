2bb9a6a3431c200c4a7b4c5050c2db130f47f3fc
Shri Radhey Mishra <148370155+ShriRadhey-Mishra@users.noreply.github.com>
2024-09-10 21:59:03 +0530

    added if_else.js

---
+++ Basics/ControlFlow/if_else.js
@@ -0,0 +1,17 @@
+let hour = 10;
+
+if (hour >= 0 && hour < 12) {
+    console.log("Good Morning, Anjalii!!");
+}
+
+else if (hour >= 12 && hour < 17) {
+    console.log("Good Afternoon, Anjalii!!");
+}
+
+else if (hour >= 17 && hour < 20) {
+    console.log("Good Evening, Anjalii!!");
+}
+
+else {
+    console.log("Good Night, Anjalii!!");
+}
