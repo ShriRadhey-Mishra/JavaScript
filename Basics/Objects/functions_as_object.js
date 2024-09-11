a666de8ba73ea1972c9b4f2e9425e8ca1ab492fe
Shri Radhey Mishra <148370155+ShriRadhey-Mishra@users.noreply.github.com>
2024-09-12 01:24:01 +0530

    added functions_as_objects.js

--- Basics/Objects/functions_as_object.js
+++ Basics/Objects/functions_as_object.js
@@ -8,4 +8,18 @@ function Circle(radius) {
 const circle = new Circle(10);
 console.log(Circle.name);   // returns the name of the function
 console.log(Circle.length); // returns the number of arguments
-console.log(Circle.constructor) // 
+console.log(Circle.constructor) // Returns the constructor that was used to create this function object
+
+// Creating new object using the parent constructor Function()
+const Circle1 = new Function('radius', 
+    `this.radius = radius;
+     this.draw = function() {
+         console.log('draw');
+    }`);
+
+const newCircle = new Circle1(100);
+
+console.log(newCircle);
+
+Circle.call({}, 1); // This is logically reuivalent to line 8 code. 
+Circle.apply({}, [1, 2, 3]); // This is same as call function but it has and array for multiple arguments.
