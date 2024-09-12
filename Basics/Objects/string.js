5d23c5fde1ff9ca395ced1b0faa75e84bee57f6b
Shri Radhey Mishra <148370155+ShriRadhey-Mishra@users.noreply.github.com>
2024-09-12 09:30:30 +0530

    updated string.js

--- Basics/Objects/string.js
+++ Basics/Objects/string.js
@@ -1,2 +1,5 @@
 // for any string manipulation use follow this link
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String
+
+// for date related functions, 
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date
