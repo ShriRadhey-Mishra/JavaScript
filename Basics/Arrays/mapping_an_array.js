d3c69ace0d5fc4f47a2e8e234463a517c4d113b9
Shri Radhey Mishra <148370155+ShriRadhey-Mishra@users.noreply.github.com>
2024-09-13 10:30:39 +0530

    Update mapping_an_array.js

--- Basics/Arrays/mapping_an_array.js
+++ Basics/Arrays/mapping_an_array.js
@@ -2,6 +2,9 @@ const numbers = [ 1, -1, 2, 3 ];
 
 const items = numbers
                 .filter( value => value>=0 )
-                .map( value => ({ value }));
+                .map( value => ({ value }))
+                .filter( object => object.value>1)
+                .map( obj => obj.value );
 
 console.log(items);
+console.log(numbers);
