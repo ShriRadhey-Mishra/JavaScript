a1714024719c16badfa93913b98c5d20ebd32195
ShriRadhey-Mishra <radheymishra13@gmail.com>
2025-02-19 20:32:23 +0530

    adding changes in util.js

--- Basics/Refresher-optional/assets/scripts/util.js
+++ Basics/Refresher-optional/assets/scripts/util.js
@@ -1,3 +1,4 @@
+// # Import and Export keyword
 // If you want to export something from the same file to the other files use this export keyword.
 // export let apiKey = "kshd92713hyro2nc8yg";
 
@@ -7,4 +8,19 @@
 // You can also have multiple named exports and you can import both of them using comma.
 // export let apikey = "ou1hd82j9h3u";
 // export let myName = "Shri Radhey Mishra";
+
+// ----------------------------------------------------------------------------------------------------
+
+// # Simple JavaScript Function
+// function combine(a, b, c) {
+//     const newValue = a * b / c;
+//     return newValue;
+// }
+//
+// const value = combine(1,2,3);
+// console.log(value);
+
+
+
+// ----------------------------------------------------------------------------------------------------
 
