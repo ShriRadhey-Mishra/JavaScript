85df651283cba6ae47aeafd62ddf68b9b339bccf
ShriRadhey-Mishra <radheymishra13@gmail.com>
2025-02-19 21:39:32 +0530

    JavaScript Refresher - Done

--- Basics/Refresher-optional/assets/scripts/util.js
+++ Basics/Refresher-optional/assets/scripts/util.js
@@ -20,7 +20,63 @@
 // const value = combine(1,2,3);
 // console.log(value);
 
+// # Arrow Function
+// const num =  number => {
+//     number = 10
+//     console.log(`Hello World! ${number}`)
+// }
+//
+// num()
 
+// # Classes in JavaScript
+// class User {
+//     constructor(name, age) {
+//         this.name = name;
+//         this.age = age;
+//     }
+//
+//     greet() {
+//         console.log("Hello World!");
+//     }
+// }
+//
+// const user1 = new User("Radhey", 21)
+// user1.greet();
+
+// # Arrays in JavaScript
+// const hobbies = ["Sports", "Reading", "Swimming"];
+// hobbies.push("Working");
+// console.log(hobbies);
+//
+// const updatedHobbies = hobbies.map (item => item + "!")
+// console.log(updatedHobbies);
+
+// let numberArray = [1, 2, 3];
+// function transformToObjects(numberArray) {
+//     // Todo: Add your logic
+//     // should return an array of objects
+//     const newArray = [];
+//     for (let item of numberArray) {
+//         newArray.push({val: item});
+//     }
+//     return newArray;
+// }
+//
+// let newArray = transformToObjects(numberArray);
+// console.log(newArray);
+
+// # Object Destructuring
+// const [firstName, lastName] = ["Shri Radhey", "Mishra"]
+// console.log(firstName);
+// console.log(lastName);
+//
+// const {firstname, nameLast: lastname} = {
+//     firstname: "Shri Radhey",
+//     nameLast: "Mishra"
+// }
+//
+// console.log(firstname);
+// console.log(lastname);
 
 // ----------------------------------------------------------------------------------------------------
 
